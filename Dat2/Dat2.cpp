7f44fbd1d1305cda20772a38c0a1b27ab9a16119
Dat <tiendat0901941322@gmail.com>
2026-04-03 10:00:09 +0700

    Change the program

--- Dat2/Dat2.cpp
+++ Dat2/Dat2.cpp
@@ -2,5 +2,18 @@
 #include <fstream>
 using namespace std;
 int main() {
-	const PI = 
+	const PI = 3.141592653589792;
+	const N = 10;
+	double Area, Perimeter;
+	double R[N];
+	int i;
+	for (i = 0, i < N, i++) :
+		cout << "Enter the radius of the circle: ";
+		cin >> R[i];
+		cout << "i = " << (i + 1) << "R[i] = " << R[i] << endl;
 }
+ofstream myfile ("AREA.DAT");
+for (i = 0, i < N, i++) :
+	Area = PI * R[i] * R[i];
+	Perimeter = 2 * PI * R[i];
+	myfile << ()

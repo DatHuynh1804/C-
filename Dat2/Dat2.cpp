e7fd919ec135921d8059a46fb8cb642c0a57453f
Dat <tiendat0901941322@gmail.com>
2026-04-03 07:56:27 +0700

    Add project files.

---
+++ Dat2/Dat2.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main() {
+	double PI = 3.1415926535897932;
+	double R[10]; //Declare an array of length 10, starting from 0 and ending at 9. R[0] = 0
+	double area, perimeter;
+	int i;
+	R[0] = 2.18;
+
+}

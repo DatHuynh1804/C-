ade7f042c572c684140f617084190ac58435b2b3
Dat <tiendat0901941322@gmail.com>
2026-04-07 15:41:01 +0700

    Full

--- Dat2/Dat2.cpp
+++ Dat2/Dat2.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <fstream>
+#include <iomanip>
 using namespace std;
 int main() {
 	const double PI = 3.1415926535897932;
@@ -7,16 +8,19 @@ int main() {
 	double R[N];
 	double Area, Perimeter;
 	for (int i = 0; i < N; i++) {
-		cout << "Enter your radius: " << '\n';
+		cout << "Enter your radius: ";
 		cin >> R[i];
-		cout << "i = " << i + 1 << "R[i] = " << R[i] << '\n';
+		cout << "i = " << i + 1 << " R[i] = " << R[i] << '\n';
 	}
-	ofstream myFile ('Dat.AREA.PERIMETER');
+	ofstream myFile("Dat.AREA");
+	myFile << fixed << setprecision(10);
 	for (int i = 0; i < N; i++) {
 		Area = PI * R[i] * R[i];
 		Perimeter = 2 * PI * R[i];
-		myFile << i + 1
-		}
+		myFile << i + 1 << ") R = " << R[i] << "\n"
+		<< "Area = " << Area << "\n"
+		<< "Perimeter = " << Perimeter << "\n";
+	}
 	myFile.close();
 	return 0;
 }

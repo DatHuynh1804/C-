f2413942c1b35874f44806f5cd0d41c03bcb4ff8
Dat <tiendat0901941322@gmail.com>
2026-04-07 16:10:06 +0700

    Full

--- Dat2/Dat2.cpp
+++ Dat2/Dat2.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
-#include <fstream>
-#include <iomanip>
+#include <fstream> //Stands for File tream
+#include <iomanip> //Stands for Input/Output manipulators
 using namespace std;
 int main() {
 	const double PI = 3.1415926535897932;
@@ -13,13 +13,12 @@ int main() {
 		cout << "i = " << i + 1 << " R[i] = " << R[i] << '\n';
 	}
 	ofstream myFile("Dat.AREA");
-	myFile << fixed << setprecision(10);
 	for (int i = 0; i < N; i++) {
 		Area = PI * R[i] * R[i];
 		Perimeter = 2 * PI * R[i];
-		myFile << i + 1 << ") R = " << R[i] << "\n"
-		<< "Area = " << Area << "\n"
-		<< "Perimeter = " << Perimeter << "\n";
+		myFile << defaultfloat << setprecision(6) << i + 1 << ") R = " << R[i] << "\n";
+		myFile << fixed << setprecision (4) << "Area = " << Area << "\n"
+			   << "Perimeter = " << Perimeter << "\n";
 	}
 	myFile.close();
 	return 0;
